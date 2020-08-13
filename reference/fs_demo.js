e028a8e59717149567d4620b2c52f2f78e0a1d77
Cheikhani <skibnewrim@gmail.com>
2020-08-13 16:41:03 -0700

    read from a file

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -1,11 +1,17 @@
 const path = require('path');
 const fs = require('fs');
 
-fs.mkdir(path.join(__dirname,'testFolder'),{}, err => {
-    if (err) console.log('ERROR ...');
-});
+// fs.mkdir(path.join(__dirname,'testFolder'),{}, err => {
+//     if (err) console.log('ERROR ...');
+// });
 
 // create and write to file 
-fs.writeFile(path.join(__dirname,'testFolder','test.txt'),'Hello node', err => {
+// fs.writeFile(path.join(__dirname,'testFolder','test.txt'),'Hello node', err => {
+//     if (err) console.log('ERROR ...');
+// });
+
+// Read file
+fs.readFile(path.join(__dirname,'testFolder','test.txt'), 'utf8', (err,data) => {
     if (err) console.log('ERROR ...');
+    console.log(data);
 });
