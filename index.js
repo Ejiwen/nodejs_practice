d513f36ab6784499ac7da7c0658d5b22a80f2cc7
Cheikhani <skibnewrim@gmail.com>
2020-08-20 19:14:43 -0700

    creat server with routing

--- index.js
+++ index.js
@@ -1,13 +1,57 @@
-// const {createServer} = require('http');
+const {createServer} = require('http');
+const fs = require('fs');
+const path = require('path');
 
-const fileOperation = require('./utils/file_utils.js');
+function serv(req,res){
 
-fileOperation.createFile("files",'test2.html');
+    let content = path.join(__dirname,"pages",
+    req.url === '/' ? "home" : req.url);
+    let extname = path.extname(content);
 
-// function serv(req,res){
-//     res.write('Hello World',req);
-//     res.end();
-// }
+    let contentType = "text/html";
 
-// let server = createServer(serv);
-// server.listen(8080, () => console.log('server listening ...'));
+    switch (extname) {
+        case('.css') : contentType = 'text/css';
+        break;
+        case('.js') : contentType = 'text/javascript';
+        break;
+        case('.jpg') : contentType = 'image/jpg';
+        break;
+        case("") : content = content+".html";
+    }
+
+
+    
+
+    fs.readFile(content, (err,data) => {
+        if(err) {
+            res.writeHead(404,{'Content-Type': "text/html"});
+            fs.readFile(path.join(__dirname,"pages","404.html"), (err,pg) => {
+                res.end(pg);
+            })
+               
+                
+        } else {
+            res.writeHead(200,{'Content-Type': contentType});
+            res.end(data);
+        }
+        
+    })
+    
+
+}
+
+
+    let server = createServer(serv);
+    server.listen(8080, () => console.log('server listening ...'));
+
+
+
+
+// const fileOperation = require('./utils/file_utils.js');
+// fileOperation.createFile("files",'test2.html');
+
+// res.writeHead(404,{'Content-Type': contentType});
+// res.end(
+//     fs.readFile(path.join(__dirname,"pages","404.html"))
+//     );
